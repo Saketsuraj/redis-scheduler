3f05eba7dac404ad1ef3972c2070aae627b36cd1
backendinternopenapp <backendintern@openapp.co>
2020-09-28 05:13:56 +0530

    Redis Scheduler Using Nodejs

---
+++ index.js
@@ -0,0 +1,12 @@
+var Scheduler = require('redis-scheduler');
+var scheduler = new Scheduler({ host: 'localhost', port: 6379 });
+var expirationTime = 8000;
+
+function eventTriggered(err, key) {
+  console.log('Notification: '+ key + ' triggered');
+}
+
+scheduler.schedule({ key: 'test-key', expire: expirationTime, handler: eventTriggered }, function (err) {
+  // Schedule set
+  console.log("Event scheduled, Please wait for the key to expire");
+});
